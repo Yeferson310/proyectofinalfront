ea6bee5f75ecdc795a284cceea40a039f3f55359
RODRIGO DE CASTRO <rodrigodecastro94@icloud.com>
2023-02-23 11:23:41 -0300

    bottones

--- src/Componentes/Header.jsx
+++ src/Componentes/Header.jsx
@@ -1,27 +1,52 @@
 import "./Header.css";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 function Header(props) {
-    return (
-        <div className="header">
-        <div className="logo-title">
-          <img src="/img/Pokeball.png" alt="Logo Pokeball" />
-          <h1>Pokédex</h1>
-        </div>
-        <div className="botoningresar">
-        <Link to={"login"}>Ingresar</Link>
-        </div><br />
-        <div className="botoningresar2">
-        <Link to={"crearpokemon"}>Crearpokemon</Link>
-        </div>
+  // localStorage.getItem("lastname");
+  const [logueado, setLogueado] = useState(false);
+  const navigate = useNavigate();
+  useEffect(function () {
+    if (localStorage.getItem("logueado") === "true") {
+      setLogueado(true);
+    } else {
+      setLogueado(false);
+    }
+  }, []);
+  function salir() {
+    localStorage.setItem("logueado", false);
+    navigate(0);
 
-  
-        <div onClick={props.ordenar}>
-          {
-            props.sorted?<img src="/img/downHastag.png" alt="downArrow" />:<img src="/img/az.png" alt="downArrow" />
-          }
-          
-        </div>
-      </div>
-    );
   }
-  export default Header;
+  return (
+    <div className="header">
+      <div className="logo-title">
+        <img src="/img/Pokeball.png" alt="Logo Pokeball" />
+        <h1>Pokédex</h1>
+      </div>
+      <div className="botoningresar">
+        <Link to={"login"}>Ingresar</Link>
+      </div>
+      <br />
+      {logueado ? (
+        <div className="botoningresar2">
+          <Link to={"crearpokemon"}>Crearpokemon</Link>
+        </div>
+      ) : (
+        ""
+      )}
+
+      <div className="botoningresar2" >
+        <button onClick={salir}> Salir</button>
+      </div>
+      <div onClick={props.ordenar}>
+        {props.sorted ? (
+          <img src="/img/downHastag.png" alt="downArrow" />
+        ) : (
+          <img src="/img/az.png" alt="downArrow" />
+        )}
+      </div>
+    </div>
+  );
+}
+export default Header;
--- src/Componentes/Login.jsx
+++ src/Componentes/Login.jsx
@@ -1,27 +1,59 @@
-import React,{ useRef,useEffect,useState } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import "./Login.css";
 
+//Importamos useHistory para navegar
+import { useNavigate } from "react-router-dom";
 
 function Login(props) {
   const usuario = useRef();
   const clave = useRef();
+  const navigate = useNavigate();
 
- function autenticar(){
-  let valorUsuario = usuario.current.value;
-  let valorClave = clave.current.value;
-  alert("autenticando..."+ valorUsuario + valorClave);
- }
-  return (
-    <div className="contenedor" >
-      <label className="usuario">usuario:</label>
-      <input type="text" ref={usuario}/>
-      <label className="password">clave:</label>
-      <input type="password"ref={clave}/>
-      <button onClick={autenticar}>Ingresar</button>
-      
-    </div>
+  function autenticar() {
+    let valorUsuario = usuario.current.value;
+    let valorClave = clave.current.value;
+    let data = { usuario: valorUsuario, clave: valorClave };
+    fetch("http://localhost:3001/autenticar", {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        Accept: "application/json",
+      },
+    })
+      .then(function (response) {
+        if (response.status === 200) {
+          return response.json();
+        } else {
+          return { msg: " usuario no autorizado" };
+        }
+      })
+
+      .then(function (myJson) {
+        if (myJson.msg === "OK") {
+          loginExitoso();
+        } else {
+          alert(myJson.msg); // utilizar un modal 
+        }
+
     
+      });
+  }
+  function loginExitoso() {
+    localStorage.setItem("logueado", true);
+    navigate("/");
+  }
+  return (
+    <div className="contenedor">
+      <label className="usuario">usuario:</label>
+      <input type="text" ref={usuario} />
+      <label className="password">clave:</label>
+      <input type="password" ref={clave} />
+      <button onClick={autenticar}>Ingresar</button>
+    </div>
   );
 }
 export default Login;
